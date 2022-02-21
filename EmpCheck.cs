8a379308352afa3a1a68d8e2d8659d8177190abf
Harry <hareshwarp649>
2022-02-21 19:31:09 +0530

    class1

---
+++ EmpCheck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWagePro
+{
+    internal class EmpCheck
+    {
+      public static void EmpPresentOrNot()
+        {
+            Random random = new Random();
+            int num = random.Next(0,2);
+            if (num == 1)
+            {
+                Console.WriteLine("Employee Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee Absent");
+            }
+        } 
+    }
+}
