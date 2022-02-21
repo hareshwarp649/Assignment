8fdb986b43ef0f09d04f3cc585a68ab47bd4bb06
Harry <hareshwarp649>
2022-02-21 18:34:19 +0530

    Update Program.cs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWagePro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Console.WriteLine("Welcome to Employee Wage Program");
+            //EmpCheck.EmpPresentOrNot();
+            // CalculateEmpWage.CalEmpWage();
+            // SwitchCalEmpWage.CalEmpWage();
+            //  HourseCalEmpWage.CalHourse(); 
+            // SnakeLadderProb.SimuLadder();
+            ;
+            LineCompaProb.ComparisonProb();
+        }
+    }
+}
